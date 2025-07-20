1bbf93363ac723077b5a0a07a4e4635fc956abb3
Deepak Yadav <yadavdeepak9686@gmail.com>
2025-07-20 13:23:05 +0530

    Update auth.js

--- Frontend/backlink-project/src/api/auth.js
+++ Frontend/backlink-project/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://linkoback.onrender.com";
 
  const registerUser = async (formData) => {
   try {
