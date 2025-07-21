e860df02371a7fd4a473d37e51d7eb73c7801386
Deepak Yadav <yadavdeepak9686@gmail.com>
2025-07-21 19:09:49 +0530

    Update razorpay.js

--- src/Config/razorpay.js
+++ src/Config/razorpay.js
@@ -1,21 +1,23 @@
-// import Razorpay from "razorpay"
-
-// export const razorpay = new Razorpay({
-
-//    key_id: process.env.RAZORPAY_KEY_ID,
-//    key_secret: process.env.RAZORPAY_KEY_SECRET
-   
-// })
 
 import Razorpay from "razorpay";
 
-export const razorpay = () => {
-  if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_SECRET) {
-    throw new Error("Razorpay keys missing in environment");
-  }
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
 
-  return new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET,
-  });
-};
+
+
+
+// import Razorpay from "razorpay";
+
+// export const razorpay = () => {
+//   if (!process.env.RAZORPAY_KEY_ID || !process.env.RAZORPAY_SECRET) {
+//     throw new Error("Razorpay keys missing in environment");
+//   }
+
+//   return new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_SECRET,
+//   });
+// };
